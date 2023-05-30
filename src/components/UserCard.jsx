b37603521e1fa1b27c03aa7f974cea62e00ee1b3
Antoine A <antoine_avenia@hotmail.fr>
2023-05-30 17:12:44 +0200

    add email

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,14 +4,14 @@ import UserInfo from './UserInfo.jsx'
 function UserCard () {
 
     const Users = [
-        { name: "John", commentaire: 'étudiant', connaissance: false, star: 3, smiley: 4 },
-        { name: "Paul", commentaire: 'étudiant', connaissance: true, star: 1, smiley: 2 },
-        { name: "Ringo", commentaire: 'salarié', connaissance: false, star: 5, smiley: 3 },
-        { name: "Georges", commentaire: 'étudiant', connaissance: true, star: 4, smiley: 1 },
-        { name: "Yoko", commentaire: 'étudiant', connaissance: false, star: 2, smiley: 5 },
-        { name: "Mickael", commentaire: 'salarié', connaissance: true, star: 3, smiley: 4 },
-        { name: "Elvis", commentaire: 'étudiant', connaissance: false, star: 1, smiley: 2 },
-        { name: "Freddie", commentaire: 'salarié', connaissance: true, star: 5, smiley: 3 },
+        { name: "John", commentaire: 'étudiant', connaissance: false, star: 3, smiley: 4, email: 'john@mail.com'},
+        { name: "Paul", commentaire: 'étudiant', connaissance: true, star: 1, smiley: 2, email: 'paul@mail.com' },
+        { name: "Ringo", commentaire: 'salarié', connaissance: false, star: 5, smiley: 3, email: 'ringo@mail.com' },
+        { name: "Georges", commentaire: 'étudiant', connaissance: true, star: 4, smiley: 1, email: 'ringo@mail.com' },
+        { name: "Yoko", commentaire: 'étudiant', connaissance: false, star: 2, smiley: 5, email: 'ringo@mail.com' },
+        { name: "Mickael", commentaire: 'salarié', connaissance: true, star: 3, smiley: 4, email: 'ringo@mail.com' },
+        { name: "Elvis", commentaire: 'étudiant', connaissance: false, star: 1, smiley: 2, email: 'ringo@mail.com' },
+        { name: "Freddie", commentaire: 'salarié', connaissance: true, star: 5, smiley: 3, email: 'ringo@mail.com' },
         
     ]
 
