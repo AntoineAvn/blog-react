d4b61608c8a0ef4b36aa472b7fd1594b7bd89ef2
Antoine A <antoine_avenia@hotmail.fr>
2023-05-30 17:04:34 +0200

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import UserCard from "./UserCard";
 import Banniere from "./Banniere";
+import FormBlog from "./FormBlog";
 
 function App() {
+
   return(
     <div>
       <Banniere />
       <Routes>
         <Route path="/userCard" element={<UserCard />} />
+        <Route path="/form" element={<FormBlog />} />
       </Routes>
     </div>
   )
--- src/components/Banniere.jsx
+++ src/components/Banniere.jsx
@@ -6,10 +6,11 @@ import { Link } from 'react-router-dom';
 function Banniere(){
     return (
         <div className="banniere">
-            <img src={logo} classNAme="img_logo" alt="logo" />
+            <img src={logo} className="img_logo" alt="logo" />
             <h1 className="titre">Bienvenue sur Blogeo</h1>
             <Link to="/"><button className="btn">Accueil</button></Link>
             <Link to="/usercard"><button className="btn">Card</button></Link>
+            <Link to="/form"><button className="btn">Form</button></Link>
         </div>
     );
 }
