fcad33c973948d48935f70930ea8c560012f0da7
Antoine A <antoine_avenia@hotmail.fr>
2023-05-30 17:43:51 +0200

    design banniere

--- src/components/Banniere.jsx
+++ src/components/Banniere.jsx
@@ -6,11 +6,15 @@ import { Link } from 'react-router-dom';
 function Banniere(){
     return (
         <div className="banniere">
-            <img src={logo} className="img_logo" alt="logo" />
-            <h1 className="titre">Bienvenue sur Blogeo</h1>
-            <Link to="/"><button className="btn">Accueil</button></Link>
-            <Link to="/usercard"><button className="btn">Card</button></Link>
-            <Link to="/form"><button className="btn">Form</button></Link>
+            <div>
+                <img src={logo} className="img_logo" alt="logo" />
+                <h1 className="titre">Bienvenue sur Blogeo</h1>
+            </div>
+            <div>
+                <Link to="/"><button className="btn">Accueil</button></Link>
+                <Link to="/usercard"><button className="btn">Card</button></Link>
+                <Link to="/form"><button className="btn">Form</button></Link>
+            </div>
         </div>
     );
 }
